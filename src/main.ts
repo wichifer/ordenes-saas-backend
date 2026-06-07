7bc90b1e7c270fba2f9df7f0a9fb722feda55963
wichifer <mailbox.fernandez@gmail.com>
2026-06-07 14:57:56 -0300

    feat(products): complete product CRUD with backend validation

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,10 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+});
     app.useGlobalPipes(
 
       new ValidationPipe({
